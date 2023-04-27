9c64a49cc713be76ee17ec0bc98d908a75512421
VasilenkoYurii <yura.vasilenko98@gmail.com>
2023-04-27 16:15:07 +0300

    add weather and style

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import { HomeTitle, HomeTitleBox } from './Home.styled';
+import ParticlesLines from 'components/ParticlesLines/ParticlesLines';
+import Weather from 'components/WeatherApp/WeatherApp';
 
 const HomePage = () => {
   return (
     <HomeTitleBox>
+      <ParticlesLines />
+      <Weather />
       <HomeTitle>
         Hello, this web application will allow you to store your contacts in
         your personal account after registration.
--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -3,9 +3,13 @@ import styled from '@emotion/styled';
 export const HomeTitleBox = styled.div`
   width: 100%;
   height: 100%;
+
   display: flex;
-  justify-content: center;
-  align-items: center;
+  justify-content: space-evenly;
+  padding: 40px;
+  @media screen and (max-width: 550px) {
+    padding: 25px;
+  }
 `;
 
 export const HomeTitle = styled.h1`
